6a5c79f7e30a6ba88a57c410658ed3c399d1226c
b2a3e8 <31370519+b2a3e8@users.noreply.github.com>
2019-05-07 01:19:31 +0200

    Add about page, Remove HelloWorl examples

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="title">Boopa-Dee Bappa-Dee</h1>
+    <p>Boopa-Dee Bappa-Dee is a programming language based on the vocabulary of <a target="_blank" href="https://www.youtube.com/watch?v=aS38wY1Fm34&feature=youtu.be">Peter Griffin's interpretation of the italian language</a>.</p>
+    <p>One could write entire programms in Boopa-Dee Bappa-Dee, however it's not very readable and effective.</p>
+    <p>Actually Boopa-Dee Bappa-Dee is a brainfuck derivative.</p>
+
+    <br />
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Command</th>
+          <th>Meaning</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>bappadee</td>
+          <td>increment the data pointer</td>
+        </tr>
+        <tr>
+          <td>boo</td>
+          <td>decrement the data pointer</td>
+        </tr>
+        <tr>
+          <td>boopa</td>
+          <td>increment the byte at the data pointer</td>
+        </tr>
+        <tr>
+          <td>bappa</td>
+          <td>decrement the byte at the data pointer</td>
+        </tr>
+        <tr>
+          <td>dee</td>
+          <td>output the byte at the data pointer</td>
+        </tr>
+        <tr>
+          <td>beepee</td>
+          <td>accept one byte of input, storing its value in the byte at the data pointer</td>
+        </tr>
+        <tr>
+          <td>boopadee</td>
+          <td>if pointer is zero, jump to matching baa</td>
+        </tr>
+        <tr>
+          <td>baa</td>
+          <td>if pointer is nonzero, jump to matching boopadee</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
