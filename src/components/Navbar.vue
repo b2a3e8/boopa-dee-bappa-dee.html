265de77b8737065b5b2c09028d0c706fabbfc219
b2a3e8 <31370519+b2a3e8@users.noreply.github.com>
2019-05-08 12:18:19 +0200

    Add some logic on translator page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -41,19 +41,6 @@
                         </div>
                     </div>
                 </div>
-
-                <!--<div class="navbar-end">
-                    <div class="navbar-item">
-                        <div class="buttons">
-                            <a class="button is-primary">
-                                <strong>Sign up</strong>
-                            </a>
-                            <a class="button is-light">
-                                Log in
-                            </a>
-                        </div>
-                    </div>
-                </div>-->
             </div>
         </div>
     </nav>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,23 +3,20 @@
 
     <div class="tabs">
       <ul>
-        <li class="is-active"><a>ASCII to Boopa-dee</a></li>
-        <li><a>Boopa-dee to ASCII</a></li>
+        <li :class="{ 'is-active': (mode === 'ascii-to-boopa') }"><a @click="mode = 'ascii-to-boopa'; autoDetectMode = false;">ASCII to Boopa-dee</a></li> <!-- TODO trigger translating on click -->
+        <li :class="{ 'is-active': (mode === 'boopa-to-ascii') }"><a @click="mode = 'boopa-to-ascii'; autoDetectMode = false;">Boopa-dee to ASCII</a></li> <!-- TODO trigger translating on click -->
       </ul>
     </div>
 
     <div class="columns is-desktop is-vcentered">
       <div class="column">
-        <textarea class="textarea has-fixed-size is-shadowless is-info" placeholder="Type or paste text" rows="11"></textarea>
+        <textarea v-model="input" @input="submit()" class="textarea has-fixed-size is-shadowless is-info" placeholder="Type or paste text" rows="11"></textarea>
       </div>
-      <!--<div class="column is-narrow" style="position: relative;">
-        <i class="fas fa-chevron-right" style="color: #BABABA; position: absolute; top: 50%; left: 35%;"></i>
-      </div>-->
       <div class="column is-narrow is-hidden-touch">
         <i class="fas fa-chevron-right" style="color: #BABABA;"></i>
       </div>
       <div class="column">
-        <textarea class="textarea has-fixed-size is-shadowless is-info" readonly rows="11"></textarea>
+        <textarea v-model="output" class="textarea has-fixed-size is-shadowless is-info" readonly rows="11"></textarea>
       </div>
     </div>
 
@@ -27,9 +24,39 @@
 </template>
 
 <script>
+var _ = require('lodash');
 export default {
   name: 'home',
   components: {
+  },
+  data() {
+    return {
+      autoDetectMode: true,
+      mode: 'ascii-to-boopa',
+      input: '',
+    };
+  },
+  computed: {
+    output() {
+      var result = '';
+      if (this.mode === 'ascii-to-boopa') {
+        result = this.input.replace(/./g, "x");
+      } else {
+        result = this.input.replace(/./g, "y");
+      }
+      return result;
+    },
+  },
+  methods: {
+    submit: _.debounce(function (e) {
+      if (this.autoDetectMode) {
+        if (this.input.match(/^(bappadee|boo|boopa|bappa|dee|boopadee|baa| )+/)) {
+          this.mode = 'boopa-to-ascii';
+        } else {
+          this.mode = 'ascii-to-boopa';
+        }
+      }
+    }, 500),
   }
 }
 </script>
